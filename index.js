1397696cea071791c6052c02f2d1c51905336bce
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-01-27 08:31:57 -0300

    implemente products path

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const products = require('./routes');
 
 const app = express();
+const cors = require('cors');
 
 const PORT = 3000;
 
+app.use(cors());
 app.use('/products', products);
 
 app.listen(PORT, () => {
